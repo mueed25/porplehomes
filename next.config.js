1c92d167b45b91f85fc46090d15465c6e6abc4ed
Muhammad Ayuba <mueedmuhammad605@gmail.com>
2024-02-14 06:31:46 +0100

    second to lsts

--- next.config.js
+++ next.config.js
@@ -2,12 +2,10 @@
 const nextConfig = {
     images: {
        
-        remotePatterns: [
-          {hostname: 'localhost',
-          pathname: '**',
-          port: '3000',
-          protocol: 'http'}
-        ],
+      domains: [
+        'localhost',
+        'digitalhippo-production.up.railway.app',
+      ],
       },
 }
 

e10229bac7767cc72a26e094d375f0770b3f2c96
mueed25 <109614264+mueed25@users.noreply.github.com>
2024-05-18 14:09:49 +0100

    Update next.config.js
    u

--- next.config.js
+++ next.config.js
@@ -1,29 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-// <<<<<<< HEAD
 
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'porplehomes.com',
+        hostname: 'www.porplehomes.com',
         port: '',
       },
     ],
   },
  
-    
-// =======
-// images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'http',
-//         hostname: 'porplehomes.com',
-//         port: '',
-//       },
-//     ],
-//   },
-// >>>>>>> b00b765ee1b137b79e62dba71b2ccb38ca91efe5
+
 }
 
 
